22be846788d46cfe50be6c0e3c64309d604adc17
VladyslavTodo <124445034+VladyslavTodo@users.noreply.github.com>
2023-05-01 21:09:32 +0300

    fix-hw-08-task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,9 +4,12 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-
 player.on('timeupdate', throttle(currentTime, 1000));
+
+const play = localStorage.getItem('videoplayer-current-time');
+if (play) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
 
 function currentTime(time) {
   localStorage.setItem('videoplayer-current-time', time.seconds);
